ac03770287e50908c425c4bd815db09c10d214f0
BlackTARKUS <ydoclee@gmail.com>
2020-09-29 13:21:25 -0400

    finished prototype of dynamically generated move cards

--- js/index.js
+++ js/index.js
@@ -1,6 +1,124 @@
+Vue.component('move-data-item', {
+  props: ['move'],
+  template: 
+    `<div class="card">
+      <div class="card-title">{{ move.name }}</div>
+      <img 
+        v-bind:src="'assets/img/GBVS_Move_Images/GBVS_Gran_' + move.input + '.webp'"
+        class="card-img"
+        v-bind:alt="move.name"
+      >
+      <div class="card-data">
+        <div class="Startup">
+          <span
+            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
+            data-tooltip="The time, measured in frames, which it takes for a move to become able to
+            strike the opponent. Startup includes the frame on which the move becomes active."
+          >
+            Startup
+          </span>
+        </div>
+        <div class="Active">
+          <span
+            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
+            data-tooltip="The  duration of time, measured in frames, during which a move is able to
+            strike the opponent."
+          >
+            Active
+          </span>
+        </div>
+        <div class="Recovery">
+          <span
+            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
+            data-tooltip="The time, measured in frames, following a move's active frames during
+            which the user's actions are limited, either partially or entirely."
+          >
+            Recovery
+          </span>
+        </div>
+        <div>
+          <span
+            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
+            data-tooltip="The type of guard which must be used to avoid getting hit by this move."
+          >
+            Guard
+          </span>
+        </div>
+        <div>
+          <span
+            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
+            data-tooltip="The value by which the opponent's health will be reduced, ignoring 
+            any reductions, when struck by this move."
+          >
+            Damage
+          </span>
+        </div>
+        <div class="On-Block">
+          <span
+            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
+            data-tooltip="The amount of time, measured in frames, during which the attacker is able 
+            to act before the opponent after the opponent succesfully guards against this attack. 
+            Negative values indicate the player who guarded can act first."
+          >
+            On-Block
+          </span>
+        </div>
+        <div class="On-Hit">
+          <span
+            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
+            data-tooltip="The amount of time, measured in frames, during which the attacker is able
+            to act before the opponent after succesfully striking the opponent with this attack.
+            Negative values indicate the player who was struck can act first."
+          >
+            On-Hit
+          </span>
+        </div>
+        <div>{{ move.startup }}</div>
+        <div>{{ move.active }}</div>
+        <div>{{ move.recovery }}</div>
+        <div>{{ move.guard }}</div>
+        <div>{{ move.damage }}</div>
+        <div>{{ move.onBlock }}</div>
+        <div>{{ move.onHit }}</div>
+      </div>
+      <div class="card-description">{{ move.description }}</div>
+    </div>`
+})
+
 var vm = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    moveList: [
+      {
+        id: 0,
+        name: 'fL',
+        input: 'fL',
+        startup: 6,
+        active: 3,
+        recovery: 13,
+        guard: 'high',
+        damage: 700,
+        onBlock: '-3',
+        onHit: '+1',
+        description: `Lorem ipsum dolor, sit, amet consectetur adipisicing elit. Iure dolores harum
+          exercitationem quasi quas asperiores unde maxime libero quod officia, accusamus adipisci 
+          voluptatibus consectetur placeat, saepe eius ducimus corporis nobis.`
+      },
+      {
+        id: 1,
+        name: 'Overdrive Surge',
+        input: '214L',
+        startup: 13,
+        active: 2,
+        recovery: 19,
+        guard: 'high',
+        damage: 700,
+        onBlock: '-6',
+        onHit: '-2',
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, officiis.
+          Corporis hic doloremque quis, possimus sed itaque voluptates, earum culpa nesciunt 
+          voluptas harum! Exercitationem, aut? Debitis laudantium dignissimos sint doloremque!`
+      }
+    ]
   }
 })
