14516819d0de22c40912722455428996cc2c3f23
ターケス (Cody) Lee <ydoclee@gmail.com>
2020-10-18 18:06:07 -0400

    full overhaul

--- js/index.js
+++ js/index.js
@@ -2,91 +2,44 @@ Vue.component('move-data-item', {
   props: ['move'],
   template: 
     `<div class="card">
-      <div class="card-title">
-        {{ move.name }}
-        <div class="subtitle" v-if="move.isSpecial == 'yes'">{{ move.input }}</div>
+      <div class="card-tabs">
+        <ul class="tabs tabs-fixed-width">
+          <li class="tab"><a href="#" @click="changeVersion" class="active">Light</a></li>
+          <li class="tab"><a href="#" @click="changeVersion">Medium</a></li>
+          <li class="tab"><a href="#" @click="changeVersion">Plus</a></li>
+        </ul>
       </div>
-      <img 
-        v-bind:src="'assets/img/GBVS_Move_Images/GBVS_Gran_' + move.input + '.webp'"
-        class="card-img"
-        v-bind:alt="move.name"
-      >
-      <div class="card-data">
-        <div class="Startup">
-          <span
-            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
-            data-tooltip="The time, measured in frames, which it takes for a move to become able to
-            strike the opponent. Startup includes the frame on which the move becomes active."
-          >
-            Startup
-          </span>
+      <div class="card-image">
+        <div  class="card-video">
+          <video v-if="move.hasVideo" autoplay muted loop playsinline><source v-bind:src="'assets/video/GBVS_Gran_' + move.input + '.mp4'" type="video/mp4"></video>
+          <img v-else v-bind:src="'assets/img/move_images/GBVS_Gran_' + move.input + '.webp'">
         </div>
-        <div class="Active">
-          <span
-            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
-            data-tooltip="The  duration of time, measured in frames, during which a move is able to
-            strike the opponent."
-          >
-            Active
-          </span>
-        </div>
-        <div class="Recovery">
-          <span
-            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
-            data-tooltip="The time, measured in frames, following a move's active frames during
-            which the user's actions are limited, either partially or entirely."
-          >
-            Recovery
-          </span>
-        </div>
-        <div>
-          <span
-            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
-            data-tooltip="The type of guard which must be used to avoid getting hit by this move."
-          >
-            Guard
-          </span>
-        </div>
-        <div>
-          <span
-            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
-            data-tooltip="The value by which the opponent's health will be reduced, ignoring 
-            any reductions, when struck by this move."
-          >
-            Damage
-          </span>
-        </div>
-        <div class="On-Block">
-          <span
-            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
-            data-tooltip="The amount of time, measured in frames, during which the attacker is able 
-            to act before the opponent after the opponent succesfully guards against this attack. 
-            Negative values indicate the player who guarded can act first."
-          >
-            On-Block
-          </span>
-        </div>
-        <div class="On-Hit">
-          <span
-            class="features simptip-position-top simptip-smooth simptip-multiline simptip-movable"
-            data-tooltip="The amount of time, measured in frames, during which the attacker is able
-            to act before the opponent after succesfully striking the opponent with this attack.
-            Negative values indicate the player who was struck can act first."
-          >
-            On-Hit
-          </span>
-        </div>
-        <div>{{ move.startup }}</div>
-        <div>{{ move.active }}</div>
-        <div>{{ move.recovery }}</div>
-        <div>{{ move.guard }}</div>
-        <div>{{ move.damage }}</div>
-        <div>{{ move.onBlock }}</div>
-        <div>{{ move.onHit }}</div>
+        <span class="card-title">{{ move.name }}</span>
       </div>
-      <div class="card-chart-container">
+      <div>
+        <div class="flex-container">
+        <div class="flex-item green-text"><span class="tooltipped underlined" data-tooltip="The time, measured in frames, which it takes for a move to become able to
+        strike the opponent. Startup includes the frame on which the move becomes active.">Startup</span><br>{{ move.startup }}</div>
+        <div class="flex-item red-text"><span class="tooltipped underlined" data-tooltip="The  duration of time, measured in frames, during which a move is able to
+        strike the opponent.">Active</span><br>{{ move.active }}</div>
+        <div class="flex-item blue-text"><span class="tooltipped underlined" data-tooltip="The time, measured in frames, following a move's active frames during
+        which the user's actions are limited, either partially or entirely.">Recovery</span><br>{{ move.recovery }}</div>
+        <div class="flex-item"><span class="tooltipped underlined" data-tooltip="The type of guard which must be used to avoid getting hit by this move.">Guard</span><br>{{ move.guard }}</div>
+        <div class="flex-item"><span class="tooltipped underlined" data-tooltip="The value by which the opponent's health will be reduced, ignoring 
+        any reductions, when struck by this move.">Damage</span><br>{{ move.damage }}</div>
+        <div class="flex-item"><span class="tooltipped underlined" data-tooltip="The amount of time, measured in frames, during which the attacker is able 
+        to act before the opponent after the opponent succesfully guards against this attack. 
+        Negative values indicate the player who guarded can act first.">On-Block</span><br>{{ move.onBlock }}</div>
+        <div class="flex-item"><span class="tooltipped underlined" data-tooltip="The amount of time, measured in frames, during which the attacker is able
+        to act before the opponent after succesfully striking the opponent with this attack.
+        Negative values indicate the player who was struck can act first.">On-Hit</span><br>{{ move.onHit }}</div>
+        <div class="flex-item"><span class="tooltipped underlined" data-tooltip="The type of invulnerability the attack grants against opposing attacks.">Invulnerabilty</span><br>{{ move.invuln }}</div>
+        </div>
+      </div>
+      <div class="card-content bordered-top" style="overflow-x: auto; overflow-y: hidden; padding: 4px; 2px">
         <svg
-          xmlns="http://www.w3.org/2000/svg" 
+          height="12px" width="500px"
+          xmlns="http://www.w3.org/2000/svg"
           :style="{ width: (move.startup + move.active + move.recovery) * 14 + 8 + 'px'}"
         >
           <rect 
@@ -104,7 +57,9 @@ Vue.component('move-data-item', {
           ></rect>
         </svg>
       </div>
-      <div class="card-description">{{ move.description }}</div>
+      <div class="card-content bordered-top">
+        {{ move.description }}
+      </div>
     </div>`
 })
 
@@ -116,15 +71,16 @@ Vue.component('character-icon', {
     }
   },
   template: 
-    `<span class="characterIcon" v-bind:character="character">
-      <img 
-        v-bind:src="'assets/img/GBVS_Character_Icons/' + character + '.webp'" 
-        loading="lazy" 
-        v-bind:alt="character + ' Icon'"
-      >
-      <a v-bind:href="'https://www.dustloop.com/wiki/index.php?title=GBVS/' + character">
-        {{ character }}
-      </a>
+    `<span v-bind:character="character">
+      <a target="_Blank" v-bind:href="'https://www.dustloop.com/wiki/index.php?title=GBVS/' + character">
+        <img
+          class="characterIcon"
+          v-bind:src="'assets/img/character_icons/' + character + '.webp'"
+          loading="lazy" 
+          link="'https://www.dustloop.com/wiki/index.php?title=GBVS/' + character"
+          v-bind:alt="character + ' Icon'"
+        >
+        {{ character }}</a>
     </span>`
 })
 
@@ -134,19 +90,19 @@ var vm = new Vue({
     moveList: [
       {
         id: 0,
-        name: 'f.L',
-        input: 'fL',
-        startup: 6,
-        active: 3,
-        recovery: 13,
+        name: 'Rising Sword L',
+        input: '623L',
+        startup: 7,
+        active: 8,
+        recovery: 22,
         guard: 'high',
-        damage: 700,
-        onBlock: '-3',
-        onHit: '+1',
-        isSpecial: 'no',
-        description: `Lorem ipsum dolor, sit, amet consectetur adipisicing elit. Iure dolores harum
-          exercitationem quasi quas asperiores unde maxime libero quod officia, accusamus adipisci 
-          voluptatibus consectetur placeat, saepe eius ducimus corporis nobis.`
+        damage: "700, 300",
+        onBlock: '-17',
+        onHit: 'KD',
+        hasVersions: 'true',
+        hasVideo: true,
+        invuln: 'full',
+        description: `Gran performs a jumping sword attack over his head. Gran is fully invulnerable during the startup of the attack.`
       },
       {
         id: 1,
@@ -159,7 +115,9 @@ var vm = new Vue({
         damage: 700,
         onBlock: '-6',
         onHit: '-2',
-        isSpecial: 'yes',
+        hasVersions: true,
+        hasVideo: true,
+        invuln: 'none',
         description: `Gran dashes forward with a slash. Overdrive Surge L is the safest variant of
         Overdrive Surge at close range. This mvoe can cancel into Overdrive Surge M on hit or block.
         On hit, 214L > 214M serves as his standard meterless combo ender. On block this move can be
@@ -168,22 +126,20 @@ var vm = new Vue({
       },
       {
         id: 2,
-        name: 'Overdrive Surge M',
-        input: '214M',
-        startup: 16,
-        active: 13,
+        name: 'Reigenleiv L',
+        input: '236L',
+        startup: 15,
+        active: 1,
         recovery: 29,
         guard: 'high',
         damage: 1200,
-        onBlock: '-10',
-        onHit: 'KD',
-        isSpecial: 'yes',
-        description: `Gran dashes forward with his foot out. Overdrive Surge M covers a lot of 
-          horizontal space in front of the user. At point blank, Gran is punishable after hitting
-          the opponent with this move. At farther distances, however, it becomes safe or even
-          advantageous due to the frame advantage gained by hitting with the latest active frames of
-          the attack. On counter hit Overdrive Surge M causes a wall bounce allowing for additional 
-          follows up.`
+        onBlock: '-6',
+        onHit: '-3',
+        hasVersions: true,
+        hasVideo: true,
+        invuln: 'none',
+        description: `Gran launches a fireball from the tip of his sword. The fireball will travel in a straight, horizontal path until it either hits something, or travels off screen. 
+        This move is commonly used to generate pressure from outside of normal attack range.`
       }
     ]
   }
